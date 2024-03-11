8e9194365c0c476b5ed5b874e70ced99e1ec33e6
ShivammaKognure <shivanikognure19@gmail.com>
2024-03-11 21:34:45 +0530

    commit

---
+++ src/Patterns/Rhombus.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Rhombus {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //Solid Rhombus
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
