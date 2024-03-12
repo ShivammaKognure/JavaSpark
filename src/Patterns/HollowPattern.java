4d6d8dd5239e09e9e89740f22726b364b2c8130a
ShivammaKognure <shivanikognure19@gmail.com>
2024-03-12 20:57:09 +0530

    commit

--- src/Patterns/HollowPattern.java
+++ src/Patterns/HollowPattern.java
@@ -39,16 +39,26 @@ public class HollowPattern {
             System.out.println();
         }
         //Hollow triangle
-        for(int i=0;i<n;i++){
+        for(int i=1;i<=n;i++){
             for(int j=0;j<n-i;j++){
                 System.out.print(" ");
             }
             for(int j=0;j<=i;j++){
-
+                if(i==n||j==0) {
                     System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
-            for(int j=0;j<i;j++){
-                System.out.print("*");
+            for(int j=1;j<i;j++){
+                if(j==1) {
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+
             }
             System.out.println();
         }
